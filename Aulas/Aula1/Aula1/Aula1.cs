aff6cee84baec36ec941e1b77a2eb6ac18ae016f
CavMCarolina <rm552925@fiap.com.br>
2025-08-26 13:57:46 -0300

    Upload da Aula1

---
+++ Aulas/Aula1/Aula1/Aula1.cs
@@ -0,0 +1,31 @@
+﻿using System; //biblioteca do sistema
+
+// Separa os blocos
+namespace Aula1
+{
+    internal class Aula1
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Aula 1 - teste");
+
+            // Escreve sem enter
+            Console.Write("aaaaaaaaaaaaaaaaaaaaaaaaaaa");
+
+            Console.WriteLine("adadasdadsa");
+
+            // Sonzinho
+            //Console.Beep(000, 1);
+
+            // Espera o usuário apertar uma tecla para sair (terminal estava fechando muito rápido)
+            //Console.WriteLine("Pressione qualquer tecla para sair...");
+            //Console.ReadLine();
+        }
+    }
+}
