c353da8e17b074b07dfb745dc7aa50d263febde1
CavMCarolina <rm552925@fiap.com.br>
2025-08-26 14:06:10 -0300

    Upload Aula 3 + Exercício

---
+++ Aulas/Aula3/Aula3/Aula3/Aula3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Aula3
+{
+    internal class Aula3
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+            int r;
+
+            Console.Write("Insira o valor para o n1: ");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Insira o valor para o n2: ");
+            n2 = int.Parse(Console.ReadLine());
+
+            r = n1 + n2;
+
+            if (r == 0)
+            {
+                Console.WriteLine("O resultado é zero");
+            }
+            if (r < 0)
+            {
+                Console.WriteLine("O resultado é negativo");
+            }
+            if (r > 0)
+            {
+                Console.WriteLine("O resultado é positivo");
+            }
+
+            // Espera o usuário apertar uma tecla para sair (terminal estava fechando muito rápido)
+            //Console.WriteLine("Pressione qualquer tecla para sair...");
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ Aulas/Aula3/ExAula3/ExAula3/ExAula3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ExAula3
+{
+    internal class ExAula3
+    {
+        static void Main(string[] args)
+        {
+            int i1, i2;
+
+            Console.Write("Insira a idade do aluno 1: ");
+            i1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Insira a idade do aluno 2: ");
+            i2 = int.Parse(Console.ReadLine());
+
+            if (i1 == i2)
+            {
+                Console.WriteLine("Os alunos têm a mesma idade");
+            }
+            if (i1 > i2)
+            {
+                Console.WriteLine("O aluno 1 é mais velho que o aluno 2");
+            }
+            if (i2 > i1)
+            {
+                Console.WriteLine("O aluno 2 é mais velho que o aluno 1");
+            }
+
+            // Espera o usuário apertar uma tecla para sair (terminal estava fechando muito rápido)
+            //Console.WriteLine("Pressione qualquer tecla para sair...");
+            //Console.ReadLine();
+        }
+    }
+}
