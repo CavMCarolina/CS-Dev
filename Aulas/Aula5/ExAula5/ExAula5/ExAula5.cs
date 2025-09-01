103a2b14b55bb8bba881fde1433772670336f3f4
CavMCarolina <rm552925@fiap.com.br>
2025-09-01 18:45:32 -0300

    Exercício --> Pirâmide com while

---
+++ Aulas/Aula5/ExAula5/ExAula5/ExAula5.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+// Separa os blocos
+namespace ExAula5
+{
+    internal class ExAula5
+    {
+        static void Main(string[] args)
+        {
+            // Looping duplo --> pirâmide
+            // a = altura; l = linha; e = espaço
+            
+            //for (int a = 0; a < 10; a++)
+            //{
+            //    for (int e = 10; e >= a; e--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+            //    for (int l = 0; l <= a; l++)
+            //    {
+            //        Console.Write("* ");
+            //    }
+
+            //    // pula para a proxima linha
+            //    Console.WriteLine();
+            //}
+
+            int a = 0;
+
+            while(a  < 10) 
+            {
+                for (int e = 10; e >= a; e--)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int l = 0; l <= a; l++)
+                {
+                    Console.Write("* ");
+                }
+
+                // pula para a proxima linha
+                Console.WriteLine();
+
+                a++;
+            }
+        }
+    }
+}
