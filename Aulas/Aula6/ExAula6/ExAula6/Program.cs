1299557dfe38389c1c78ceac8d8f0156b3325186
Carolina <cavm.carolina@gmail.com>
2025-09-15 11:17:05 -0300

    Exercicio da Aula 6 --> senha

---
+++ Aulas/Aula6/ExAula6/ExAula6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System; //biblioteca do sistema
+
+// Separa os blocos
+namespace ExAula6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // array de números --> senha
+            string[] listaSenha = { "5", "4", "6", "9" };
+
+            while (true)
+            {
+                Console.WriteLine("\n~~~ Tente descobrir a senha ~~~\nDigite 4 números separados por um espaço");
+                string[] entrada = Console.ReadLine().Split(' ');
+
+
+                for (int i = 0; i < listaSenha.Length; i++)
+                {
+                    // Verifica se o usuário digitou 4 números
+                    if (entrada.Length != listaSenha.Length)
+                    {
+                        Console.WriteLine("Por favor, digite exatamente 4 números.");
+                        break;
+                    }
+
+                    if (entrada[i] != listaSenha[i])
+                    {
+                        Console.WriteLine("Senha incorreta! Tente novamente :c");
+                        break;
+                    }
+
+                    if (i == listaSenha.Length - 1)
+                    {
+                        Console.WriteLine("Parabéns, você acertou!");
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
