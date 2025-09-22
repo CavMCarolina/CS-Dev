c74914ec8359ff9c46c1bf38d0b18c7f947c6de2
Carolina <cavm.carolina@gmail.com>
2025-09-22 11:24:30 -0300

    Aula 7 - Orientação a Objeto

---
+++ Aulas/Aula7/Aula7/Aula7/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Aula7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Veiculo veiculo = new Veiculo(123456);
+            veiculo.Ligar();
+            veiculo.Placa = "ABC-1234";
+        }
+    }
+}
---
+++ Aulas/Aula7/Aula7/Aula7/Veiculo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula7
+{
+    internal class Veiculo
+    {
+        private string placa;
+
+        private int codFrabrica;
+
+        public Veiculo(int codFrabrica)
+        {
+            this.codFrabrica = codFrabrica;
+        }
+
+        // getter e setter --> visual studio cria automatico
+        public string Placa { get => placa; set => placa = value; }
+
+        // outro jeito de fazer getter e setter
+        //public string Placa
+        //{
+        //    get { return placa; }
+        //    set { placa = value; }
+        //}
+
+        // getter e setter pequeno
+        //public string Placa { get; set; }
+
+        public void Ligar()
+        {
+            Console.WriteLine("O veículo está ligado!");
+        }
+    }
+}
