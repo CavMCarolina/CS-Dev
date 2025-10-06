a71dad60c787d32cd6fd919b2b5ba6138b9c662a
Carolina <cavm.carolina@gmail.com>
2025-10-06 11:41:14 -0300

    Encapsulamento

---
+++ CPs/CP2/ConceitosFundamentais/Encapsulamento/Encapsulamento/ContaBancaria.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulamento
+{
+    internal class ContaBancaria
+    {
+        private double saldo;
+        private string titular;
+        private string numeroConta;
+        private string agencia;
+        private string banco;
+
+        public double Saldo { get; set; }
+        public string Titular { get; set; }
+        public string NumeroConta { get; set; }
+        public string Agencia { get; set; }
+        public string Banco { get; set; }
+
+        public ContaBancaria(string titular, string numeroConta, string agencia, string banco)
+        {
+            this.titular = titular;
+            this.numeroConta = numeroConta;
+            this.agencia = agencia;
+            this.banco = banco;
+        }
+
+        public void Exibir()
+        {
+            Console.WriteLine($"Titular: {titular}\nBanco: {banco}\nAgência: {agencia}\nNúmero da Conta: {numeroConta}\nSaldo: R${Saldo}\n");
+        }
+    }
+}
---
+++ CPs/CP2/ConceitosFundamentais/Encapsulamento/Encapsulamento/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Encapsulamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta = new ContaBancaria("João Silva", "12345-6", "0001", "Banco Exemplo");
+            conta.Exibir(); // saldo = 0 por padrão
+            conta.Saldo = 150.75; // usando o setter para definir o saldo
+            conta.Exibir(); // saldo atualizado
+        }
+    }
+}
