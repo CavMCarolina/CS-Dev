96b04c207e05ed4ea492b8a8819a960be98c7736
Carolina <cavm.carolina@gmail.com>
2025-10-06 11:15:12 -0300

    Tópico 5 - Livro

---
+++ CPs/CP2/Livro/Livro/Livro.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Livro
+{
+    internal class Livro
+    {
+        string titulo;
+        string autor;
+        int anoPublicacao;
+
+
+        public Livro(string titulo, string autor,int anoPublicacao)
+        {
+            this.titulo = titulo;
+            this.autor = autor;
+            this.anoPublicacao = anoPublicacao;
+        }
+
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine("~~~~ Livro ~~~~");
+            Console.WriteLine($"{titulo} - {autor} ({anoPublicacao})");
+        }
+    }
+}
---
+++ CPs/CP2/Livro/Livro/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Livro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Livro livro = new Livro("Um Livro Qualquer", "Autor Bom", 1997);
+
+            livro.ExibirInformacoes();
+        }
+    }
+}
